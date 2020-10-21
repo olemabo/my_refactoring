f122787fa363a6d5513a76ac8e51f4c5dd753e80
olemabo <olemartinbo@gmail.com>
2020-10-21 12:26:49 +0200

    Added comments

--- RefactoringSample1/Movie.cs
+++ RefactoringSample1/Movie.cs
@@ -12,18 +12,27 @@ namespace RefactoringSample1
 
 		private string _title;
 		private Price _price;
-		//private int _priceCode;
 
+		/// <summary>
+		/// Constructor initilizing a movie object with correct movie name and price object
+		/// corresponding to the given price code. 
+		/// </summary>
+		/// <param name="title">string: title of movie</param>
+		/// <param name="priceCode">int: price code of movie</param>
 		public Movie(string title, int priceCode)
 		{
 			_title = title;
 			SetPriceCode(priceCode);
-			//_priceCode = priceCode;
 		}
 
+		/// <summary>
+		/// Set the _price object to a correct price object based on the price code.
+		/// A switch statement will sett the matching price code to correct price object
+		/// if it finds one, otherwise it will be set to the default object (RegularPrice()). 
+		/// </summary>
+		/// <param name="PriceCode">int: the price code of the movie</param>
 		public void SetPriceCode(int PriceCode)
         {
-			// return price 
 			_price = PriceCode switch
 			{
 				CHILDRENS => new ChildrenPrice(),
@@ -33,26 +42,39 @@ namespace RefactoringSample1
 			};
         }
 
+		/// <summary>
+		/// Get the price code associated with a movie object 
+		/// </summary>
+		/// <returns>int: price code</returns>
 		public int GetPriceCode()
 		{
 			return _price.GetPriceCode();
 		}
 
+		/// <summary>
+		/// Get the charge of renting a movie which is calculated based on the number of days rented
+		/// </summary>
+		/// <param name="daysRented">Number of days which a film has been rented</param>
+		/// <returns>double: The charge of renting a movie for daysRented number of days</returns>
 		public double GetCharge(int daysRented)
         {
 			return _price.GetCharge(daysRented);
         }
 
+		/// <summary>
+		/// Get number of frequent points which is calculated based on the number of days rented
+		/// </summary>
+		/// <param name="daysRented">Number of days which a film has been rented</param>
+		/// <returns>the number of frequent points</returns>
 		public int GetFrequentPoints(int daysRented)
 		{
 			return _price.GetFrequentPoints(daysRented);
 		}
 
-		//public static void SetPriceCode(int arg)
-		//{
-		//	_priceCode = arg;
-		//}
-
+		/// <summary>
+		/// Get the title of the movie from the movie class
+		/// </summary>
+		/// <returns>string: title of the movie</returns>
 		public string GetTitle()
 		{
 			return _title;
